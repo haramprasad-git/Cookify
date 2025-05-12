40029aea952ca401e49e0ce42fbdc3f63732d890
Hara M Prasad <haramprasad.09@gmail.com>
2025-05-12 11:38:10 +0530

    Updated the Procfile to run the collectstatic command also

--- Cookify/Cookify/settings.py
+++ Cookify/Cookify/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'Cookify.urls'
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 
 # Whitenoise Static constants
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 # Media files
 
